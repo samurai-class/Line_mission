e65f7c63eaf83d41b212604c040675303f14282f
Junpei <dpsknu25@gmail.com>
2021-01-19 23:55:22 +0900

    20210119

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ $Fp = fopen("data_1.txt", "r");
 $Result_Code = 0;
 
 // 1回目のデータ読み込み /////////////////////////////////////////////////////
+echo "1行目のデータ<br>";
+
 $Data = fgets($Fp);
 $Value = explode(' ', $Data);
 // 現在の時刻
@@ -11,8 +13,9 @@ $Current_TimeStamp = $Value[0];
 // 前回記録時からの走行距離
 $Mileage_Since_Last_Time_m = $Value[1] ;
 // 画面表示
-echo "現在の時刻：".$Current_TimeStamp."<br>";
-echo "前回記録時からの走行距離：".$Mileage_Since_Last_Time_m." [m]<br><br>";
+DisplayOnScreen($Current_TimeStamp, "現在の時刻");
+DisplayOnScreen($Mileage_Since_Last_Time_m, "前回記録時からの走行距離[m]");
+echo "<br>";
 
 //2回目のデータを読みこむ前に前回の時刻を保存する変数を更新
 $Last_TimeStamp = $Current_TimeStamp;
@@ -30,32 +33,39 @@ $Sum_of_Mileage = (float)0;
 $Base_Fare_Mileage = 1052;
 
 for ($Num = 1; $Num < $Max_Num; $Num++){
-    
-    $Data = fgets($Fp);
 
-    // 読み込むデータがない場合、処理を終了
+
+    echo ($Num+1)."行目のデータ<br>";
+
+
+    // 一番上のデータを読み込む >> データがない場合、繰り返し処理を終了
+    $Data = fgets($Fp);
     if($Data == null){
         break;
     }
 
+
+    // 読み込んだデータを分割し、それぞれ変数に代入
     $Value = explode(' ', $Data);
+    $Current_TimeStamp = $Value[0];             // 現在の時刻
+    $Mileage_Since_Last_Time_m = $Value[1] ;    // 前回記録時からの走行距離
+    // DisplayOnScreen($Current_TimeStamp, "現在の時刻");
+    // DisplayOnScreen($Mileage_Since_Last_Time_m, "前回記録時からの走行距離[m]");
 
-    // 現在の時刻
-    $Current_TimeStamp = $Value[0];   
-
-     // 前回記録時からの走行距離
-    $Mileage_Since_Last_Time_m = $Value[1] ;
-
-    // 一番初めは前回記録時の時刻が無い為、差分が出ないように調整する
-    // if($Num == 0){
-    //     $Last_TimeStamp = $Current_TimeStamp;
-    // }
 
     // 前回記録時からの時間を算出
-    $diff_seconds = (strtotime($Current_TimeStamp) - strtotime($Last_TimeStamp));
+    $diff_seconds = (strtotime($Current_TimeStamp) - strtotime($Last_TimeStamp));   // 秒表記
+    $diff_hour = $diff_seconds / 3600 ;                                             // 時間表記
+    // DisplayOnScreen(strtotime($Current_TimeStamp), "現在の時刻(計算ができる形式)");
+    // DisplayOnScreen(strtotime($Last_TimeStamp), "一つ前の時刻(計算ができる形式)");
+    // DisplayOnScreen((int)number_format($diff_seconds), "前回記録時からの時間[秒]");
+    // DisplayOnScreen((int)number_format($diff_seconds), "前回記録時からの時間[時間]");
 
+
+    //前回記録時からの時速を算出
     $Mileage_Since_Last_Time_km = (double)$Mileage_Since_Last_Time_m / 1000 ;
-    $diff_hour = $diff_seconds / 3600 ;
+    // DisplayOnScreen(number_format($kmph,2), "前回記録時からの時速[km/h]");
+
 
     // 低速走行時間の合計を算出
     $kmph = $Mileage_Since_Last_Time_km / $diff_hour ;
@@ -71,14 +81,11 @@ for ($Num = 1; $Num < $Max_Num; $Num++){
         $Sum_of_Mileage += (float)($Mileage_Since_Last_Time_m*1.25);
     }
 
-    // 画面表示
-    echo "現在の時刻：".$Current_TimeStamp."<br>";
-    echo "前回記録時からの走行距離：".$Mileage_Since_Last_Time_m." [m]<br>";
-    echo "前回記録時からの時間：".(int)number_format($diff_seconds)." [秒]<br>";
-    echo "時速：".number_format($kmph,2)."[km/h]<br><br>";
+
 
     // 次の処理に移る前に"前回の記録時刻"を更新
     $Last_TimeStamp = $Current_TimeStamp;
+    echo "<br>";
 }
 
     //運賃を算出する/////////////////////////////////////////////////////////
@@ -107,7 +114,11 @@ echo "運賃：".(int)$Sum_of_Fare."[円]<br>";
 function CalculationFare(){
     echo "";
 }
-
+function DisplayOnScreen($a_Variable, $a_text){
+    echo "　";
+    var_dump($a_Variable);
+    echo "　<---　".$a_text."<br>";
+}
 
 
 ?>
